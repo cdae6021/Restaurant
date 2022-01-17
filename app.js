357bf7cc6cb98cba7a1725810c9521c03f900a45
cdae6021 <cdae6021@yahoo.com.tw>
2022-01-17 17:33:43 +0800

    add create function

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 const restaurantData = require('./models/restaurant')
+const bodyParser = require('body-parser')
+const restaurant = require('./models/restaurant')
 const app = express()
 const port = 3000
 const db = mongoose.connection
@@ -21,6 +23,8 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 
@@ -43,14 +47,55 @@ app.get('/restaurants/:_id', (req, res) => {
         .catch(error => console.log(error))
 })
 
+//搜尋功能
 app.get('/search', (req, res) => {
     const keyword = req.query.keyword.toLowerCase()
-    const restaurant = restaurants.results.filter((item) => {
-        return item.name.toLowerCase().includes(keyword) || item.category.toLowerCase().includes(keyword)
-    })
-    res.render('index', { restaurant, keyword })
+    restaurantData
+        .find()
+        .lean()
+        .then(item => {
+            const keywordFilter = item.filter(item => item.name.includes(keyword) || item.category.includes(keyword))
+            res.render('index', { item: keywordFilter, keyword })
+        })
+        .catch(error => console.log(error))
 })
 
+//編輯功能
+app.get('/restaurants/:_id/edit', (req, res) => {
+    const id = req.params._id
+    restaurantData
+        .findById(id)
+        .lean()
+        .then(item => res.render('edit', { item }))
+        .catch(error => console.log(error))
+})
+
+app.post('/restaurants/:_id/edit', (req, res) => {
+    const id = req.params._id
+    restaurantData
+        .findById(id)
+        .then(item => {
+            item.category = req.body.category
+            item.location = req.body.location
+            item.phone = req.body.phone
+            item.description = req.body.description
+            return item.save()
+        })
+        .then(() => res.redirect(`/restaurants/${id}`))
+        .catch(error => console.log(error))
+})
+
+//新增餐廳
+app.get('/new', (req, res) => {
+    res.render('new')
+})
+
+app.post('/new', (req, res) => {
+    return restaurantData
+        .create(req.body)
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
+})
 
 app.listen(port, () => {
     console.log(`success , http:/localhost:${port}`)
