f22cbf40ecd9ad87d1452149bcf707a982b4f76e
cdae6021 <cdae6021@yahoo.com.tw>
2022-01-17 15:49:55 +0800

    add detail page

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-const restaurants = require('./restaurant.json')
 const mongoose = require('mongoose')
-const Restaurant = require('./models/restaurant')
+const restaurantData = require('./models/restaurant')
 const app = express()
 const port = 3000
 const db = mongoose.connection
@@ -18,19 +17,30 @@ db.once('open', () => {
 })
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+
 app.set('view engine', 'handlebars')
+
 app.use(express.static('public'))
 
 
+
+//餐廳清單首頁
 app.get('/', (req, res) => {
-    res.render('index', { restaurant: restaurants.results })
+    restaurantData
+        .find()
+        .lean()
+        .then(item => res.render('index', { item }))
+        .catch(error => console.log(error))
 })
 
-app.get('/restaurants/:id', (req, res) => {
-    const restaurant = restaurants.results.find(item => {
-        return item.id.toString() === req.params.id
-    })
-    res.render('show', { restaurant })
+//個別餐廳資訊
+app.get('/restaurants/:_id', (req, res) => {
+    const id = req.params._id
+    restaurantData
+        .findById(id)
+        .lean()
+        .then(item => res.render('show', { item }))
+        .catch(error => console.log(error))
 })
 
 app.get('/search', (req, res) => {
